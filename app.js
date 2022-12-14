b5ceb7eae76087cec5c50ba055ab2d15f9e820b6
Miles Collins <milescollins9@gmail.com>
2022-12-14 16:41:28 -0700

    added interaction

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-document.addEventListener
+document.querySelector('button').addEventListener('click', submit)
+
+function submit(){
+    let newName = document.getElementById('frontCardName').value
+    let newCardNum = document.getElementById('frontCardNumber').value
+    let newExp = document.getElementById('frontCardExpMonth').value
+    let newYr = document.getElementById('frontCardExpYr').value
+    let capsNewName = newName.toUpperCase()
+    document.querySelector('#cardName').innerText = capsNewName
+    document.querySelector('#cardNumber').innerText = newCardNum
+    document.querySelector('#cardExp').innerText = newExp 
+    document.querySelector('#cardYr').innerText = newYr
+}
