edad9fc74f51652735e5c248ee3931878ff51de0
Miles Collins <milescollins9@gmail.com>
2022-12-16 23:29:49 -0700

    added the complete page

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-document.querySelector('button').addEventListener('click', submit)
+document.getElementById('submitButton').addEventListener('click', submit)
 
 
 
 function submit(){
+    console.log('submit')
     // the following lines of code declare the variables that will be put into the card
 let newName = document.getElementById('frontCardName').value
 let newCardNum = document.getElementById('frontCardNumber').value
@@ -10,7 +11,9 @@ let newExp = document.getElementById('frontCardExpMonth').value
 let newYr = document.getElementById('frontCardExpYr').value
 let newCvc = document.getElementById('frontCardCvc').value
 let capsNewName = newName.toUpperCase()
+let x = document.getElementById('formId')
 const element = document.querySelector('.formSectionBorder')
+let formComplete = document.getElementById('formComplete')
 
 // these lines of code will be actually replacing the default characters in the card
     document.querySelector('#cardName').innerText = capsNewName
@@ -18,6 +21,8 @@ const element = document.querySelector('.formSectionBorder')
     document.querySelector('#cardExp').innerText = newExp 
     document.querySelector('#cardYr').innerText = newYr
     document.querySelector('#cvc').innerText = newCvc
-
+    x.classList.add('hidden')
+    formComplete.classList.toggle('hidden')
+    
 }
 
