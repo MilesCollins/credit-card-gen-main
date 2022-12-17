c5d026fbd59973371237968578b31eb6421eeef1
Miles Collins <milescollins9@gmail.com>
2022-12-16 21:19:42 -0700

    added more functionality

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 document.querySelector('button').addEventListener('click', submit)
 
+
+
 function submit(){
-    let newName = document.getElementById('frontCardName').value
-    let newCardNum = document.getElementById('frontCardNumber').value
-    let newExp = document.getElementById('frontCardExpMonth').value
-    let newYr = document.getElementById('frontCardExpYr').value
-    let capsNewName = newName.toUpperCase()
+    // the following lines of code declare the variables that will be put into the card
+let newName = document.getElementById('frontCardName').value
+let newCardNum = document.getElementById('frontCardNumber').value
+let newExp = document.getElementById('frontCardExpMonth').value
+let newYr = document.getElementById('frontCardExpYr').value
+let newCvc = document.getElementById('frontCardCvc').value
+let capsNewName = newName.toUpperCase()
+const element = document.querySelector('.formSectionBorder')
+
+// these lines of code will be actually replacing the default characters in the card
     document.querySelector('#cardName').innerText = capsNewName
     document.querySelector('#cardNumber').innerText = newCardNum
     document.querySelector('#cardExp').innerText = newExp 
     document.querySelector('#cardYr').innerText = newYr
+    document.querySelector('#cvc').innerText = newCvc
+
 }
+
